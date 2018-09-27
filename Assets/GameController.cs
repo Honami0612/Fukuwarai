5563151f63831cf2a3230b2c01dbd60364236abb
shai04 <cisa16081@g.nihon-u.ac.jp>
2018-09-27 21:44:29 +0900

    1

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -8,15 +8,29 @@ public class GameController : MonoBehaviour {
      float speedX = 0;
     float speedY = 0;
     Vector2 startPos;
+    GameObject parts1;
+    GameObject parts2;
+    GameObject parts3;
+    GameObject parts4;
+    GameObject parts5;
+    GameObject parts6;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+
+        parts1 = GameObject.Find("parts1");
+        parts2 = GameObject.Find("parts2");
+        parts3 = GameObject.Find("parts3");
+        parts4 = GameObject.Find("parts4");
+        parts5 = GameObject.Find("parts5");
+        parts6 = GameObject.Find("parts6");
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	 void Update() {
 
+        
         if (Input.GetMouseButtonDown(0))
         {
             this.startPos = Input.mousePosition;
@@ -27,8 +41,8 @@ public class GameController : MonoBehaviour {
 
             float swipeLengthX = endPos.x - startPos.x;
             float swipeLengthY = endPos.y - startPos.y;
-            this.speedX = swipeLengthX / 500.0f;
-            this.speedY = swipeLengthY / 500.0f;
+            this.speedX = swipeLengthX / 250.0f;
+            this.speedY = swipeLengthY / 250.0f;
         }
 
         
@@ -37,4 +51,5 @@ public class GameController : MonoBehaviour {
         this.speedY *= 0.98f;
 
 	}
+
 }
---
+++ Assets/Script/Game/MoveScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+
+    public bool trigger;
+    private Vector2 startPos;
+    private Vector3 inputPos;
+
+    private Collider collider;
+     
+    [SerializeField]
+    float speedX = 0;
+    [SerializeField]
+    float speedY = 0;
+
+	// Use this for initialization
+	void Start () {
+        trigger = false;
+        collider = this.gameObject.GetComponent<Collider>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trigger == true)
+        {
+
+            if (Input.GetMouseButtonUp(1))
+            {
+                Vector2 endPos = Input.mousePosition;
+
+                float swipeLengthX = endPos.x - startPos.x;
+                float swipeLengthY = endPos.y - startPos.y;
+                this.speedX = swipeLengthX / 250.0f;
+                this.speedY = swipeLengthY / 250.0f;
+            }
+
+
+            transform.Translate(this.speedX, this.speedY, 0);
+            this.speedX *= 0.98f;
+            this.speedY *= 0.98f;
+
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray tapPosition = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            
+            if (collider.Raycast(tapPosition, out hit, 10F))
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                Debug.Log("aaaa");
+                if (hit.collider.gameObject.name == this.gameObject.name)
+                {
+                    OnClickObject();
+                }
+            }           
+        }
+
+    }
+
+        public void OnClickObject()
+    {
+        trigger = !trigger;
+    }
+
+}
