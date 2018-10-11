a285218e0d1721def1bc8845386bf243b71a0293
shai04 <cisa16081@g.nihon-u.ac.jp>
2018-10-11 21:43:42 +0900

    1

--- Assets/Script/Game/GameController.cs
+++ Assets/Script/Game/GameController.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    GameObject partsPrefab;
+
+    [SerializeField]
+    Sprite[] partsSprite;
+
+    private int num;
+
+
+    private void Start()
+    {
+        num = 0;
+
+        GameObject.Find("parts").GetComponent<SpriteRenderer>().sprite = partsSprite[num];
+    }
+
+
+    public void Generate()
+    {
+        Debug.Log("確認");
+        num++;
+        GameObject mouth = Instantiate(partsPrefab) as GameObject;
+        mouth.GetComponent<SpriteRenderer>().sprite = partsSprite[num];
+ //       mouth.transform.localPosition = new Vector3(0, 0, 0);
+
+    }
+
 }
