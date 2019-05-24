fa120542de0b7cf98793a74bbeec3117e18e9cb3
Honami0612 <ciho16007@g.nihon-u.ac.jp>
2019-05-25 04:06:38 +0900

    test

--- Assets/Script/Lobby/CreateNewRoom.cs
+++ Assets/Script/Lobby/CreateNewRoom.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CreateNewRoom : MonoBehaviour {
 
@@ -23,7 +24,7 @@ public class CreateNewRoom : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-       
+   
     }
 
 
@@ -32,20 +33,29 @@ public class CreateNewRoom : MonoBehaviour {
     {
         switch (number)
         {
-            case 0://InputField.GetComponent<Text>().text = "";
+            case 0://CreateNewRoomButton
+            //InputField.GetComponent<Text>().text = "";
             InputField.SetActive(true);
                 DecisionButton.gameObject.SetActive(true);
                 break;
 
 
-            case 1:
+            case 1://DecisionButton
                 lobbyManager.RoomCreate();
                 InputField.SetActive(false);
                 DecisionButton.gameObject.SetActive(false);
 
                 break;
-            case 2:
-               // lobbyManager.JoinRoom();
+            case 2://GameStartButtom(masteronly)
+                Debug.Log("masterbuttonclick");
+                //SceneManager.LoadScene("Start");
+                break;
+            case 3://UpdateButtom
+                lobbyManager.UpDateRoom();
+
+                break;
+            case 4:
+                lobbyManager.LeaveRoom();
                 break;
         }
 
--- Assets/Script/Lobby/LobbyManager.cs
+++ Assets/Script/Lobby/LobbyManager.cs
@@ -5,80 +5,151 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 // Canvasにつける
-public class LobbyManager : MonoBehaviour {
-  [SerializeField]
-  Text message;
+public class LobbyManager : MonoBehaviour
+{
+    [SerializeField]
+    Text message;
 
-  // roomNameを書くとこ
-  [SerializeField]
-  GameObject roomNameArea;
+    // roomNameを書くとこ
+    [SerializeField]
+    GameObject roomNameArea;
 
-  //今あるroom一覧
-  [SerializeField]
-  GameObject roomListArea;
-  private List<GameObject> roomList;
-  [SerializeField]
-  GameObject roomPrefub;
 
-  //入る　or 作る　ルーム名
-  private string roomName;
+    //今あるroom一覧
+    [SerializeField]
+    GameObject roomListArea;
+    public List<GameObject> roomList;
+    [SerializeField]
+    GameObject roomPrefub;
 
-  private void Start(){
-    PhotonNetwork.ConnectUsingSettings("バージョン番号");
-  }
+    
+    public Button GameStart;
+
+    //入る　or 作る　ルーム名
+    private string roomName;
+
+
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("バージョン番号");
+        UpDateRoom();
+        GameStart.gameObject.SetActive(false);
+    }
 
     private void Update()
     {
 
     }
 
+    public void UpDateRoom()
+    {
+        int i = 0;
+        foreach(GameObject obj in roomList)
+        {
+            Destroy(roomList[i].gameObject);
+            i++;
+        }
+
+        roomList.Clear();
+
+        i = 0;
+        RoomInfo[] roomInfos = PhotonNetwork.GetRoomList();
+        if (roomInfos.Length == 0)
+        {
+            message.text = "ルームがありません";
+        }
+        else
+        {
+            foreach (RoomInfo room in roomInfos)
+            {
+                roomList.Add(Instantiate(roomPrefub,transform));
+                roomList[i].transform.parent = roomListArea.transform;
+                //roomList[i].transform.SetParent(roomListArea.transform);
+                roomList[i].GetComponentsInChildren<Text>()[0].text = room.Name;
+                int j = i;
+                roomList[i].GetComponent<Button>().onClick.AddListener(() => RoomJoin(j));
+                i++;
+
+            }
+        }
+        message.text = "ルームを更新しました";
+    }
+
     // ルーム作成
-    public void RoomCreate(){
+    public void RoomCreate()
+    {
         //roomNameをTextからとる
-        roomName = GameObject.Find("RoomNameArea").GetComponent<InputField>().text;
+        roomName = GameObject.Find("InputRoomName").GetComponent<InputField>().text;
         Debug.Log(roomName);
-   //roomName = roomNameArea.GetComponent<Text>().text;//上で取れる？
-    //roomを作成できた時の処理
-    if(PhotonNetwork.CreateRoom(roomName)){
-    /*  roomList.Add(Instantiate(roomPrefub));
-            // 作成したルームの設定
-            int number = roomList.Count - 1;
-      roomList[number].transform.parent = roomListArea.transform;
-     roomList[number].GetComponent<Button>().onClick.AddListener( () => JoinRoom(number) );
-     *///ここはスタートで呼ぶ
-      //message.text = "ルームの作成に成功しました";
+        //roomを作成できた時の処理
+        if (PhotonNetwork.CreateRoom(roomName))
+        {
+            message.text = "ルームの作成に成功しました";
             Debug.Log("ルーム作成に成功しました");
-    }else{
-      //message.text = "ルームの作成に失敗しました";
+        }
+        else
+        {
+            message.text = "ルームの作成に失敗しました";
             Debug.Log("ルーム作成に失敗しました");
+        }
     }
-  }
 
-  // ルームに入室する
-  public void JoinRoom(int number){
-    roomName = roomList[number].GetComponent<Text>().text;
-    if(PhotonNetwork.JoinRoom(roomName)){
-        message.text = "ルームに入室しました";
-    }else{
-      message.text = "ルームに入室できませんでした";
+    // ルームに入室する
+    public void RoomJoin(int number)
+    {
+        roomName = roomList[number].GetComponentsInChildren<Text>()[0].text;
+        if (PhotonNetwork.JoinRoom(roomName))
+        {
+            message.text = "ルームに入室しました";
+
+        }
+        else
+        {
+            message.text = "ルームに入室できませんでした";
+        }
     }
-  }
 
-  // ルームから退出する時使って
-  // public void LeaveRoom(){
-  //   if (PhotonNetwork.LeaveRoom()){
-  //
-  //   }else{
-  //
-  //   }
-  // }
-  
-  public void ToGameScene(){
-    SceneManager.LoadScene("Game");
-  }
+    public void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinRoom");
+        PhotonPlayer photonPlayer = PhotonNetwork.masterClient;
+        Debug.Log("Player" + photonPlayer);
+        if (photonPlayer != null)
+        {
+            GameStart.gameObject.SetActive(true);
+        }
+    }
+
+    public void OnPhotonJoinRoomFailed()
+    {
+        message.text = "ルームに入れませんでした";
+        Debug.Log("ルームに入れませんでした");
+    }
+
+    //ルームから退出する時使って
+    public void LeaveRoom()
+    {
+        if (PhotonNetwork.LeaveRoom())
+        {
+            message.text = "ルームから退出しました";
+        }
+        else
+        {
+            message.text = "ルームから退出できませんでした";
+        }
+    }
+
+    public void ToGameScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
 
 }
 //生成するプレハブ、ボタンとテキスト
-//RoomListをUpDataでまわしつづける
+//GetRoomList(PhotonNetwork.cs)をUpDateでまわしつづける
 //1回目に呼んだルーム数と2回目に呼んだルーム数が違った場合ルームの数をふやす（常に更新）
-//
+
+//start()でInstanseなどを一度まわして、更新ボタンを押した場合とーやさんの写真（元のサイト）のをまわす
+//oldと現在のを比較して違った場合のみ（元のサイト）をまわす
+
+//PhotonPlayer PhotonNetwork.masterClient //マスターかマスターでないかを判断できる。マスターがスタートボタンを押したらゲームスタート
